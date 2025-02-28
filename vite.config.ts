5db610bb1ab69a76fc6ada0a32baf330326cbc09
Egge <egge@getcurrent.io>
2025-02-28 18:05:46 +0100

    build step

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
+  plugins: [dts({ rollupTypes: true })],
   build: {
     lib: {
       entry: "src/index.ts",
-      name: "nutline",
-      fileName: "nutline",
+      name: "almnd",
+      fileName: "almnd",
     },
   },
 });
