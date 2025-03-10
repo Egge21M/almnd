8e7c02582be83b5665b7ea5d646db33b757f656d
Egge <egge@getcurrent.io>
2025-03-10 16:50:18 +0100

    bundle settings

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
     rollupOptions: {
       external: ["@cashu/cashu-ts"],
     },
+    sourcemap: true,
     lib: {
       entry: "src/index.ts",
       name: "almnd",
